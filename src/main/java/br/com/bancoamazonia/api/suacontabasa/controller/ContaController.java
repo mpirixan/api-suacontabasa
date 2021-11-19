3716f7f0038bb12fb2dd633a0fe525c6bc8a41a2
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2021-11-19 13:28:48 -0300

    Api pode ser consumida

--- src/main/java/br/com/bancoamazonia/api/suacontabasa/controller/ContaController.java
+++ src/main/java/br/com/bancoamazonia/api/suacontabasa/controller/ContaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,6 +19,7 @@ import br.com.bancoamazonia.api.suacontabasa.controller.dto.ContaResponse;
 import br.com.bancoamazonia.api.suacontabasa.domain.model.Conta;
 import br.com.bancoamazonia.api.suacontabasa.manager.ContaManager;
 
+@CrossOrigin(origins = "http://127.0.0.1:3000")
 @RestController
 @RequestMapping(value = "/contas")
 public class ContaController {
--- src/main/java/br/com/bancoamazonia/api/suacontabasa/controller/PessoaController.java
+++ src/main/java/br/com/bancoamazonia/api/suacontabasa/controller/PessoaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.bancoamazonia.api.suacontabasa.domain.model.Pessoa;
 import br.com.bancoamazonia.api.suacontabasa.manager.ContaManager;
 import br.com.bancoamazonia.api.suacontabasa.manager.PessoaManager;
 
+@CrossOrigin(origins = "http://127.0.0.1:3000")
 @RestController
 @RequestMapping(value = "/pessoas")
 public class PessoaController {
