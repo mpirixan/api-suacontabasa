a0002d3f3ea9331dd6a35b9dc47a8484bd92c941
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2021-12-17 13:41:16 -0300

    Correção de imports

--- src/main/java/br/com/bancoamazonia/api/suacontabasa/domain/model/Pessoa.java
+++ src/main/java/br/com/bancoamazonia/api/suacontabasa/domain/model/Pessoa.java
@@ -1,7 +1,6 @@
 package br.com.bancoamazonia.api.suacontabasa.domain.model;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
