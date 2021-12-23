7b6448198c960d39aaa9ff541491a7b89fc70519
Matheus Dias Pirixan <10965@adm.bancoamazonia.sa>
2021-12-23 13:41:07 -0300

    Criação no banco de dados

--- src/main/java/br/com/bancoamazonia/api/suacontabasa/domain/model/Agencia.java
+++ src/main/java/br/com/bancoamazonia/api/suacontabasa/domain/model/Agencia.java
@@ -1,5 +1,111 @@
 package br.com.bancoamazonia.api.suacontabasa.domain.model;
 
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="agencia")
 public class Agencia {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="idagencia")
+	private Long idAgencia;
+	
+	@Column(name="gestor")
+	private Long gestor;
+	
+	
+	@ManyToOne(cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+	@JoinColumn(name="conta", referencedColumnName="idConta")
+	private Conta conta;
+
+
+	@ManyToOne(cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+	@JoinColumn(name="funcionario", referencedColumnName="idFuncionario")
+	private Funcionario funcionario;
+	
+	public Agencia() {
+		
+	}
+	
+	
+	public Agencia(Long idAgencia, Long gestor, Conta conta) {
+		super();
+		this.idAgencia = idAgencia;
+		this.gestor = gestor;
+		this.conta = conta;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(conta, gestor, idAgencia);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agencia other = (Agencia) obj;
+		return Objects.equals(conta, other.conta) && Objects.equals(gestor, other.gestor)
+				&& Objects.equals(idAgencia, other.idAgencia);
+	}
+
+
+	public Long getIdAgencia() {
+		return idAgencia;
+	}
+
+
+	public void setIdAgencia(Long idAgencia) {
+		this.idAgencia = idAgencia;
+	}
+
+
+	public Long getGestor() {
+		return gestor;
+	}
+
+
+	public void setGestor(Long gestor) {
+		this.gestor = gestor;
+	}
+
+
+	public Conta getConta() {
+		return conta;
+	}
+
+
+	public void setConta(Conta conta) {
+		this.conta = conta;
+	}
+
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	
+	
 }
--- src/main/java/br/com/bancoamazonia/api/suacontabasa/domain/model/Funcionario.java
+++ src/main/java/br/com/bancoamazonia/api/suacontabasa/domain/model/Funcionario.java
@@ -1,5 +1,28 @@
 package br.com.bancoamazonia.api.suacontabasa.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="funcionario")
 public class Funcionario {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="idFuncionario")
+	private Long idFuncionario;
+
+	public Long getIdFuncionario() {
+		return idFuncionario;
+	}
+
+	public void setIdFuncionario(Long idFuncionario) {
+		this.idFuncionario = idFuncionario;
+	}
+	
+	
 }
